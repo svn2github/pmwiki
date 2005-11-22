9f322abc116b1e10ca1050dce228a66f722a6979
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2005-11-22 20:53:49 +0000

    Fixed bug in displaying (:directives:) in history.
    git-svn-id: svn://pmwiki.org/pmwiki@937 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/scripts/pagerev.php
+++ trunk/scripts/pagerev.php
@@ -110,7 +110,7 @@ function PrintDiff($pagename) {
               str_replace("\n","<br />",htmlspecialchars(join("\n",$in))),
               "</div>";
           else echo MarkupToHTML($pagename,
-            preg_replace('/\\(:(.*?):\\)/','[@$1@]',join("\n",$in)));
+            preg_replace('/\\(:.*?:\\)/e',"Keep(PSS('$0'))",join("\n",$in)));
         }
         if ($match[4]=='d' || $match[4]=='c') {
           $txt = str_replace('line',$lines,$DiffAddFmt[$match[4]]);
@@ -121,7 +121,7 @@ function PrintDiff($pagename) {
               str_replace("\n","<br />",htmlspecialchars(join("\n",$out))),
               "</div>";
           else echo MarkupToHTML($pagename,
-            preg_replace('/(\\(:.*?:\\))/','[@$1@]',join("\n",$out)));
+            preg_replace('/\\(:.*?:\\)/e',"Keep(PSS('$0'))",join("\n",$out)));
         }
         echo FmtPageName($DiffEndDelAddFmt,$pagename);
       }
