c06061832f3cd9664b81441fbfc10ae07d0c4cb0
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2006-02-06 22:09:58 +0000

    Added "ak_savedraft" key.
    git-svn-id: svn://pmwiki.org/pmwiki@1174 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/scripts/prefs.php
+++ trunk/scripts/prefs.php
@@ -1,5 +1,5 @@
 <?php if (!defined('PmWiki')) exit();
-/*  Copyright 2005 Patrick R. Michaud (pmichaud@pobox.com)
+/*  Copyright 2005-2006 Patrick R. Michaud (pmichaud@pobox.com)
     This file is part of PmWiki; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published
     by the Free Software Foundation; either version 2 of the License, or
@@ -37,5 +37,6 @@ XLSDV('en', array(
   'ak_recentchanges' => 'c',
   'ak_save' => 's',
   'ak_saveedit' => 'u',
+  'ak_savedraft' => 'd',
   'ak_preview' => 'p'));
 
