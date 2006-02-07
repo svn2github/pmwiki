59777ae8ba893b26a7983a023ee7f440a8a04ef0
pmichaud <pmichaud@524c5546-5005-0410-9a3e-e25e191bd360>
2006-02-07 02:29:21 +0000

    Allow spaces around '#' in reference links.
    git-svn-id: svn://pmwiki.org/pmwiki@1182 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/scripts/stdmarkup.php
+++ trunk/scripts/stdmarkup.php
@@ -1,5 +1,5 @@
 <?php if (!defined('PmWiki')) exit();
-/*  Copyright 2004-2005 Patrick R. Michaud (pmichaud@pobox.com)
+/*  Copyright 2004-2006 Patrick R. Michaud (pmichaud@pobox.com)
     This file is part of PmWiki; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published
     by the Free Software Foundation; either version 2 of the License, or
@@ -214,7 +214,7 @@ Markup('[[#','<[[','/(?>\\[\\[#([A-Za-z][-.:\\w]*))\\]\\]/e',
 
 ## [[target |#]] reference links
 Markup('[[|#', '<[[|',
-  "/(?>\\[\\[([^|\\]]+))\\|#\\]\\]/e",  
+  "/(?>\\[\\[([^|\\]]+))\\|\\s*#\\s*\\]\\]/e",  
   "Keep(MakeLink(\$pagename,PSS('$1'),'['.++\$MarkupFrame[0]['ref'].']'),'L')");
 
 ## [[target |+]] title links
