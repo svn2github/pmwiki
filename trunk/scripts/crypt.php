c533374127cc8a104a957eefa3e2f603c1dc203e
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2009-07-12 12:50:23 +0000

    Escape form value in crypt.php, improbable XSS vulnerability (PITS:01113)
    git-svn-id: svn://pmwiki.org/pmwiki@2361 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/scripts/crypt.php
+++ trunk/scripts/crypt.php
@@ -1,5 +1,5 @@
 <?php if (!defined('PmWiki')) exit();
-/*  Copyright 2002-2004 Patrick R. Michaud (pmichaud@pobox.com)
+/*  Copyright 2002-2009 Patrick R. Michaud (pmichaud@pobox.com)
     This file is part of PmWiki; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published
     by the Free Software Foundation; either version 2 of the License, or
@@ -19,7 +19,8 @@ function HandleCrypt($pagename, $auth='read') {
   echo FmtPageName(
     "<form action='{\$ScriptUrl}' method='POST'><p>
       Enter password to encrypt: 
-      <input type='text' name='passwd' value='$passwd' />
+      <input type='text' name='passwd' value='"
+      . htmlspecialchars($passwd, ENT_QUOTES) ."' />
       <input type='submit' />
       <input type='hidden' name='n' value='{\$FullName}' />
       <input type='hidden' name='action' value='crypt' /></p></form>",
