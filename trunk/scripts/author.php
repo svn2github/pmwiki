a542ca9dfb06e13d51092ca7b8b5d284d49361c4
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2009-12-02 21:50:49 +0000

    Fix apostrophes in Author field, PITS:01155
    git-svn-id: svn://pmwiki.org/pmwiki@2432 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/scripts/author.php
+++ trunk/scripts/author.php
@@ -1,5 +1,5 @@
 <?php if (!defined('PmWiki')) exit();
-/*  Copyright 2004-2006 Patrick R. Michaud (pmichaud@pobox.com)
+/*  Copyright 2004-2009 Patrick R. Michaud (pmichaud@pobox.com)
     This file is part of PmWiki; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published
     by the Free Software Foundation; either version 2 of the License, or
@@ -27,7 +27,7 @@ if (!isset($Author)) {
     $x = stripmagic(@$_COOKIE[$AuthorCookie]);
   } else $x = @$AuthId;
   $Author = htmlspecialchars(preg_replace("/[^$AuthorNameChars]/", '', $x), 
-                ENT_QUOTES);
+                ENT_COMPAT);
 }
 if (!isset($AuthorPage)) $AuthorPage = 
     FmtPageName('$AuthorGroup/$Name', MakePageName($pagename, $Author));
