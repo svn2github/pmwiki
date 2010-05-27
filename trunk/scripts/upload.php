baa5540ef5fd59a1458b0f6e3eede66dc4a53b53
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2010-05-27 20:29:36 +0000

    Fix HandleDownload to flush() output before exit (PITS:01199)
    git-svn-id: svn://pmwiki.org/pmwiki@2557 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/scripts/upload.php
+++ trunk/scripts/upload.php
@@ -1,5 +1,5 @@
 <?php if (!defined('PmWiki')) exit();
-/*  Copyright 2004-2009 Patrick R. Michaud (pmichaud@pobox.com)
+/*  Copyright 2004-2010 Patrick R. Michaud (pmichaud@pobox.com)
     This file is part of PmWiki; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published
     by the Free Software Foundation; either version 2 of the License, or
@@ -192,6 +192,7 @@ function HandleDownload($pagename, $auth = 'read') {
   $fp = fopen($filepath, "rb");
   if ($fp) {
     while (!feof($fp)) echo fread($fp, 4096);
+    flush();
     fclose($fp);
   }
   exit();
