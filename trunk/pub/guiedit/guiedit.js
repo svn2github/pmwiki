75224b23be2bef8f9a565b96fff0c3e310bf76ac
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2015-07-22 16:12:12 +0000

    Mod guiedit.js::insMarkup() to accept a custom function name processing the text, and a custom id for the text area.
    git-svn-id: svn://pmwiki.org/pmwiki@3087 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/pub/guiedit/guiedit.js
+++ trunk/pub/guiedit/guiedit.js
@@ -19,8 +19,19 @@ function insButton(mopen, mclose, mtext, mlabel, mkey) {
     + mlabel + "</a>");
 }
 
-function insMarkup(mopen, mclose, mtext) {
-  var tarea = document.getElementById('text');
+function insMarkup() {
+  var func = false, tid='text', mopen = '', mclose = '', mtext = '';
+  if(typeof arguments[0] == 'function') {
+    var func = arguments[0];
+    if(arguments.length > 1) tid = arguments[1];
+    mtext = func('');
+  }
+  else if (arguments.length >= 3) {
+    var mopen = arguments[0], mclose = arguments[1], mtext = arguments[2];
+    if(arguments.length > 3) tid = arguments[3];
+  }
+  
+  var tarea = document.getElementById(tid);
   if (tarea.setSelectionRange > '') {
     var p0 = tarea.selectionStart;
     var p1 = tarea.selectionEnd;
@@ -31,6 +42,7 @@ function insMarkup(mopen, mclose, mtext) {
     while (p1 > p0 && tarea.value.substring(p1-1, p1) == ' ') p1--; 
     if (p1 > p0) {
       str = tarea.value.substring(p0, p1);
+      if(func) str = func(str);
       cur0 = p0 + mopen.length + str.length + mclose.length;
       cur1 = cur0;
     }
@@ -44,7 +56,7 @@ function insMarkup(mopen, mclose, mtext) {
   } else if (document.selection) {
     var str = document.selection.createRange().text;
     tarea.focus();
-    range = document.selection.createRange()
+    range = document.selection.createRange();
     if (str == '') {
       range.text = mopen + mtext + mclose;
       range.moveStart('character', -mclose.length - mtext.length );
@@ -53,6 +65,7 @@ function insMarkup(mopen, mclose, mtext) {
       if (str.charAt(str.length - 1) == " ") {
         mclose = mclose + " ";
         str = str.substr(0, str.length - 1);
+        if(func) str = func(str);
       }
       range.text = mopen + str + mclose;
     }
