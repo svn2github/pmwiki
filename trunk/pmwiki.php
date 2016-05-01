336afca8bc86f82fcedcc3ad36d65eaa42fdc555
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2016-05-01 10:58:47 +0000

    Add $HTMLTagAttr, to allow inclusion of lang, manifest and other attributes
    git-svn-id: svn://pmwiki.org/pmwiki@3146 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/pmwiki.php
+++ trunk/pmwiki.php
@@ -169,11 +169,12 @@ $HTTPHeaders = array(
 $CacheActions = array('browse','diff','print');
 $EnableHTMLCache = 0;
 $NoHTMLCache = 0;
+$HTMLTagAttr = '';
 $HTMLDoctypeFmt = 
   "<!DOCTYPE html 
     PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
     \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
-  <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'><head>\n";
+  <html xmlns='http://www.w3.org/1999/xhtml' \$HTMLTagAttr><head>\n";
 $HTMLStylesFmt['pmwiki'] = "
   ul, ol, pre, dl, p { margin-top:0px; margin-bottom:0px; }
   code.escaped { white-space: nowrap; }
