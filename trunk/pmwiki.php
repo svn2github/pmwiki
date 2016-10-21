57e5effa80eb7400f826ca7a29df60d2a1416ef5
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2016-10-21 07:45:42 +0000

    Skip checking for $AllowedPassword if empty or false.
    git-svn-id: svn://pmwiki.org/pmwiki@3189 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/pmwiki.php
+++ trunk/pmwiki.php
@@ -2140,9 +2140,9 @@ function IsAuthorized($chal, $source, &$from) {
         if (@$AuthList[$pw]) $auth = $AuthList[$pw];
         continue;
       }
-      if (pmcrypt($AllowPassword, $pw) == $pw)           # nopass
+      if ($AllowPassword && pmcrypt($AllowPassword, $pw) == $pw) # nopass
         { $auth=1; continue; }
-      foreach((array)$AuthPw as $pwresp)                       # password
+      foreach((array)$AuthPw as $pwresp)                         # password
         if (pmcrypt($pwresp, $pw) == $pw) { $auth=1; continue; }
     }
   }
