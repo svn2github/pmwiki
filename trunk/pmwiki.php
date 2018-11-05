e2ecd07aa5a3d6a2c108685534604b23a41195eb
petko <petko@524c5546-5005-0410-9a3e-e25e191bd360>
2018-11-05 16:01:02 +0000

    Fix inconsistent white space with inline [@escaped code@], reported by Dannybpng.
    git-svn-id: svn://pmwiki.org/pmwiki@3401 524c5546-5005-0410-9a3e-e25e191bd360

--- trunk/pmwiki.php
+++ trunk/pmwiki.php
@@ -178,7 +178,7 @@ $HTMLDoctypeFmt =
   <html xmlns='http://www.w3.org/1999/xhtml' \$HTMLTagAttr><head>\n";
 $HTMLStylesFmt['pmwiki'] = "
   ul, ol, pre, dl, p { margin-top:0px; margin-bottom:0px; }
-  code.escaped { white-space: nowrap; }
+  code.escaped { white-space: pre; }
   .vspace { margin-top:1.33em; }
   .indent { margin-left:40px; }
   .outdent { margin-left:40px; text-indent:-40px; }
